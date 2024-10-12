b3dc83357e7862cbe3a89842964fdc15fd0acd70
Rahul Kumar <172767765+Rahulkr3201@users.noreply.github.com>
2024-10-12 12:48:45 +0530

    Update Feed.jsx

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -3,7 +3,7 @@ import { BASE_URL } from "../utils/constant.js";
 import { useDispatch, useSelector } from "react-redux";
 import { addFeed } from "../utils/feedSlice.js";
 import { useEffect } from "react";
-import UserCard from "./userCard";
+import UserCard from "./UserCard";
 
 const Feed = () => {
   const feed = useSelector((store) => store.feed);
