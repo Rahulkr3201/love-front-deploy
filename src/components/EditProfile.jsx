7a50a8be13b624b61e785d081a55c876fc6872c8
Rahul Kumar <172767765+Rahulkr3201@users.noreply.github.com>
2025-05-08 12:36:22 +0530

    Update EditProfile.jsx

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -4,17 +4,18 @@ import axios from "axios";
 import { useDispatch } from "react-redux";
 import { BASE_URL } from "../utils/constant.js";
 import { addUser } from "../utils/userSlice.js";
+
 const EditProfile = ({ user }) => {
-  const [firstName, setfirstName] = useState(user.firstName);
-  const [lastName, setlastName] = useState(user.lastName);
-  const [photoUrl, setphotoUrl] = useState(user.photoUrl);
-  const [age, setage] = useState(user.age || " ");
-  const [gender, setgender] = useState(user.gender || " ");
-  const [about, setabout] = useState(user.about);
+  const [firstName, setFirstName] = useState(user.firstName);
+  const [lastName, setLastName] = useState(user.lastName);
+  const [photoUrl, setPhotoUrl] = useState(user.photoUrl);
+  const [age, setAge] = useState(user.age || " ");
+  const [gender, setGender] = useState(user.gender || " ");
+  const [about, setAbout] = useState(user.about);
   const [skills, setSkills] = useState(user.skills || "");
-  const [error, seterror] = useState("");
+  const [error, setError] = useState("");
   const dispatch = useDispatch();
-  const [showToast, setshowToast] = useState(false);
+  const [showToast, setShowToast] = useState(false);
 
   const saveProfile = async () => {
     try {
@@ -31,94 +32,133 @@ const EditProfile = ({ user }) => {
         },
         { withCredentials: true }
       );
-      setshowToast(true);
+      setShowToast(true);
       setTimeout(() => {
-        setshowToast(false);
+        setShowToast(false);
       }, 3000);
 
       dispatch(addUser(res.data.data));
     } catch (err) {
-      seterror(err.message);
+      setError(err.message);
     }
   };
 
   return (
     <>
       {showToast && (
-        <div className="toast toast-top toast-center">
-          <div className="alert alert-success">
-            <span>Profile Saved Successfuly</span>
-          </div>
+        <div className="alert alert-success fixed top-4 left-1/2 transform -translate-x-1/2 w-3/4 sm:w-1/3 z-50">
+          <span>Profile Saved Successfully</span>
         </div>
       )}
-      <div className=" flex justify-evenly -scroll my-12">
-        <div className="card bg-base-300 w-1/3 shadow-xl ">
-          <div className="card-body w-3/4">
-            <h2 className="card-title text-3xl">Edit Profile </h2>
-            <div className="m-2">
-              <label className="text-sm ml-16">FirstName:</label>
-              <input
-                type="text"
-                value={firstName}
-                placeholder="First Name "
-                className=" ml-16 card bg-base-100 input input-ghost w-full max-w-xs border-blue-400"
-                onChange={(e) => setfirstName(e.target.value)}
-              />
-              <label className="text-sm ml-16 ">LastName:</label>
-              <input
-                type="text"
-                value={lastName}
-                placeholder="Last Name"
-                className=" ml-16 card bg-base-100 input input-ghost w-full max-w-xs border-blue-400"
-                onChange={(e) => setlastName(e.target.value)}
-              />
-              <label className="text-sm ml-16">PhotoUrl:</label>
-              <input
-                type="text"
-                value={photoUrl}
-                placeholder="photoUrl"
-                className=" ml-16  card bg-base-100 input input-ghost w-full max-w-xs border-blue-400"
-                onChange={(e) => setphotoUrl(e.target.value)}
-              />
-              <label className="text-sm ml-16">Age:</label>
-              <input
-                type="text"
-                value={age}
-                placeholder="Age "
-                className=" ml-16 card bg-base-100 input input-ghost w-full max-w-xs border-blue-400"
-                onChange={(e) => setage(e.target.value)}
-              />
-              <label className="text-sm ml-16">Gender:</label>
-              <input
-                type="text"
-                value={gender}
-                placeholder="Gender"
-                className=" ml-16  card bg-base-100 input input-ghost w-full max-w-xs border-blue-400"
-                onChange={(e) => setgender(e.target.value)}
-              />
-              <label className="text-sm ml-16">About:</label>
-              <textarea
-                value={about}
-                className="textarea textarea-info ml-16 w-full h-12 "
-                placeholder="about"
-                onChange={(e) => setabout(e.target.value)}
-              ></textarea>
-              <label className="text-sm ml-16">Skills:</label>
-              <textarea
-                value={skills}
-                className="textarea textarea-info ml-16 w-full h-12 "
-                placeholder="skills"
-                onChange={(e) => setSkills(e.target.value)}
-              ></textarea>
+      <div className="flex justify-center p-6 mb-12">
+        <div className="card bg-white w-full sm:w-1/3 shadow-lg mx-5 rounded-xl p-4">
+          <div className="card-body">
+            <h2 className="card-title text-3xl font-semibold text-gray-800 mb-4">
+              Edit Profile
+            </h2>
+            <div className="space-y-4">
+              <div>
+                <label className="text-sm font-medium text-gray-600">
+                  First Name:
+                </label>
+                <input
+                  type="text"
+                  value={firstName}
+                  placeholder="First Name"
+                  className="input input-bordered w-full border-gray-300 focus:ring-2 focus:ring-blue-500"
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+              </div>
+
+              <div>
+                <label className="text-sm font-medium text-gray-600">
+                  Last Name:
+                </label>
+                <input
+                  type="text"
+                  value={lastName}
+                  placeholder="Last Name"
+                  className="input input-bordered w-full border-gray-300 focus:ring-2 focus:ring-blue-500"
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+              </div>
+
+              <div>
+                <label className="text-sm font-medium text-gray-600">
+                  Photo URL:
+                </label>
+                <input
+                  type="text"
+                  value={photoUrl}
+                  placeholder="Photo URL"
+                  className="input input-bordered w-full border-gray-300 focus:ring-2 focus:ring-blue-500"
+                  onChange={(e) => setPhotoUrl(e.target.value)}
+                />
+              </div>
+
+              <div>
+                <label className="text-sm font-medium text-gray-600">
+                  Age:
+                </label>
+                <input
+                  type="text"
+                  value={age}
+                  placeholder="Age"
+                  className="input input-bordered w-full border-gray-300 focus:ring-2 focus:ring-blue-500"
+                  onChange={(e) => setAge(e.target.value)}
+                />
+              </div>
+
+              <div>
+                <label className="text-sm font-medium text-gray-600">
+                  Gender:
+                </label>
+                <input
+                  type="text"
+                  value={gender}
+                  placeholder="Gender"
+                  className="input input-bordered w-full border-gray-300 focus:ring-2 focus:ring-blue-500"
+                  onChange={(e) => setGender(e.target.value)}
+                />
+              </div>
+
+              <div>
+                <label className="text-sm font-medium text-gray-600">
+                  About:
+                </label>
+                <textarea
+                  value={about}
+                  className="textarea textarea-bordered w-full h-24 border-gray-300 focus:ring-2 focus:ring-blue-500"
+                  placeholder="About"
+                  onChange={(e) => setAbout(e.target.value)}
+                ></textarea>
+              </div>
+
+              <div>
+                <label className="text-sm font-medium text-gray-600">
+                  Skills:
+                </label>
+                <textarea
+                  value={skills}
+                  className="textarea textarea-bordered w-full h-24 border-gray-300 focus:ring-2 focus:ring-blue-500"
+                  placeholder="Skills"
+                  onChange={(e) => setSkills(e.target.value)}
+                ></textarea>
+              </div>
             </div>
-            <div className="card-actions mx-44">
-              <button className=" btn btn-success " onClick={saveProfile}>
+
+            <div className="card-actions justify-center mt-6">
+              <button
+                className="btn btn-primary w-full py-2 rounded-lg text-white hover:bg-blue-600 transition-all"
+                onClick={saveProfile}
+              >
                 Save Profile
               </button>
             </div>
           </div>
         </div>
-        <div>
+
+        <div className="ml-6">
           <UserCard
             user={{ firstName, lastName, photoUrl, gender, age, about, skills }}
           />
